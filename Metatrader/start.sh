608e6f7b7146bff6309a455f12f42f6a54c91771
Minh Nguyen <minh.nguyenvan95@gmail.com>
2025-10-30 12:59:08 +0700

    Update start.sh

--- Metatrader/start.sh
+++ Metatrader/start.sh
@@ -69,59 +69,9 @@ fi
 # Recheck if MetaTrader 5 is installed
 if [ -e "$mt5file" ]; then
     show_message "[4/7] File $mt5file is installed. Running MT5..."
-    $wine_executable "$mt5file" &
+    export WINEESYNC=1
+    export WINEFSYNC=1
+    WINEDEBUG=+seh,+loaddll,+tid,+err $wine_executable "$mt5file" &> wine_mt5.log
 else
     show_message "[4/7] File $mt5file is not installed. MT5 cannot be run."
-fi
-
-
-# Install Python in Wine if not present
-if ! $wine_executable python --version 2>/dev/null; then
-    show_message "[5/7] Installing Python in Wine..."
-    curl -L $python_url -o /tmp/python-installer.exe
-    $wine_executable /tmp/python-installer.exe /quiet InstallAllUsers=1 PrependPath=1
-    rm /tmp/python-installer.exe
-    show_message "[5/7] Python installed in Wine."
-else
-    show_message "[5/7] Python is already installed in Wine."
-fi
-
-# Upgrade pip and install required packages
-show_message "[6/7] Installing Python libraries"
-$wine_executable python -m pip install --upgrade --no-cache-dir pip
-# Install MetaTrader5 library in Windows if not installed
-show_message "[6/7] Installing MetaTrader5 library in Windows"
-if ! is_wine_python_package_installed "MetaTrader5==$metatrader_version"; then
-    $wine_executable python -m pip install --no-cache-dir MetaTrader5==$metatrader_version
-fi
-# Install mt5linux library in Windows if not installed
-show_message "[6/7] Checking and installing mt5linux library in Windows if necessary"
-if ! is_wine_python_package_installed "mt5linux"; then
-    $wine_executable python -m pip install --no-cache-dir mt5linux
-fi
-
-# Install mt5linux library in Linux if not installed
-show_message "[6/7] Checking and installing mt5linux library in Linux if necessary"
-if ! is_python_package_installed "mt5linux"; then
-    pip install --upgrade --no-cache-dir mt5linux
-fi
-
-# Install pyxdg library in Linux if not installed
-show_message "[6/7] Checking and installing pyxdg library in Linux if necessary"
-if ! is_python_package_installed "pyxdg"; then
-    pip install --upgrade --no-cache-dir pyxdg
-fi
-
-# Start the MT5 server on Linux
-show_message "[7/7] Starting the mt5linux server..."
-python3 -m mt5linux --host 0.0.0.0 -p $mt5server_port -w $wine_executable python.exe &
-
-# Give the server some time to start
-sleep 5
-
-# Check if the server is running
-if ss -tuln | grep ":$mt5server_port" > /dev/null; then
-    show_message "[7/7] The mt5linux server is running on port $mt5server_port."
-else
-    show_message "[7/7] Failed to start the mt5linux server on port $mt5server_port."
 fi
